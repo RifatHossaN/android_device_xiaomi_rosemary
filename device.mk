fb3f33c6b518ce96f2e536f74f2905ed99b735f0
CodeChas3r1 <benologajayson@gmail.com>
2023-07-14 08:19:23 +0800

    rosemary: Import XiaomiParts from begonia
    Signed-off-by: CodeChas3r1 <codechas3r@gmail.com>

--- device.mk
+++ device.mk
@@ -311,6 +311,10 @@ PRODUCT_PACKAGES += \
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+	
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
