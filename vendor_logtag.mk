fd2d5a10acabd355bf9f2a5bf3e026426d5df2a7
CodeChas3r1 <benologajayson@gmail.com>
2023-07-14 08:35:21 +0800

    rosemary: props: Silent some logspam
    E OpenGLRenderer: Unable to match the desired swap behavior.
    W ContrastColorUtil: Background should not be translucent: #ffffff
    
    Signed-off-by: CodeChas3r1 <codechas3r@gmail.com>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -277,4 +277,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.MAPI-SocketListener=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MAPI-CommandProcessor=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.RpAudioControl=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.OpenGLRenderer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.ContrastColorUtil=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL)
