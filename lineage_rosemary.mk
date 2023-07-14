bbec5e6c3e4f4aa2be47ea44c33c2bf04b91f7cc
CodeChas3r1 <benologajayson@gmail.com>
2023-07-14 08:07:24 +0800

    rosemary: Enable blur effect
    * Changes QS transparency into blur effect.
    Change-Id: 56e22365a50eae9018e5f4b95c5a9571c09fbfbb
    
    Signed-off-by: CodeChas3r1 <codechas3r@gmail.com>

--- lineage_rosemary.mk
+++ lineage_rosemary.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/rosemary/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 PRODUCT_NAME := lineage_rosemary
 PRODUCT_DEVICE := rosemary
 PRODUCT_MANUFACTURER := Xiaomi
