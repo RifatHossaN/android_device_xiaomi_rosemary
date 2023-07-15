9c4b3b905d3d6ee3d47e2830760179f8fb9a8f78
RifatHossaN <104590425+RifatHossaN@users.noreply.github.com>
2023-07-15 16:36:53 +0600

    Update cherish_rosemary.mk

--- cherish_rosemary.mk
+++ cherish_rosemary.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Blur
 TARGET_ENABLE_BLUR := true
