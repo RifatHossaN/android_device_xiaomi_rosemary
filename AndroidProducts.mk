41dc569e36859e38845126f24e8df9696e657cdb
RifatHossaN <104590425+RifatHossaN@users.noreply.github.com>
2023-07-30 07:00:24 -0500

    rifat: bringup for superior

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rosemary.mk
+    $(LOCAL_DIR)/superior_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosemary-user \
-    lineage_rosemary-userdebug \
-    lineage_rosemary-eng
+    superior_rosemary-user \
+    superior_rosemary-userdebug \
+    superior_rosemary-eng
