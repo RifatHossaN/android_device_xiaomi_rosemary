bfb59e8f917f785262ab74c085c55336408961ad
RifatHossaN <104590425+RifatHossaN@users.noreply.github.com>
2023-07-30 07:04:39 -0500

    rifat: bringup for superior

--- superior_rosemary.mk
+++ superior_rosemary.mk
@@ -12,12 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Blur
 TARGET_ENABLE_BLUR := true
 
-PRODUCT_NAME := lineage_rosemary
+PRODUCT_NAME := superior_rosemary
 PRODUCT_DEVICE := rosemary
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -25,7 +25,19 @@ PRODUCT_MODEL := Redmi Note 10S
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Some Build Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Call recording
+TARGET_SUPPORTS_CALL_RECORDING := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="rosemary-user 11 RP1A.200720.011 V12.5.16.0.RKLMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/rosemary_global/rosemary:11/RP1A.200720.011/V12.5.16.0.RKLMIXM:user/release-keys
+
+# Maintainer Flags
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.superior.maintainer=Nukx
